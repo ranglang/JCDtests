794e0926dec891a339a5bc794150d1c1422b6f21
rawqing <kingstar95d@gmail.com>
2016-09-28 11:45:05 +0800

    整个测试的花费时间计算完成

--- app/src/androidTest/java/com/trubuzz/trubuzz/feature/AdRunner.java
+++ app/src/androidTest/java/com/trubuzz/trubuzz/feature/AdRunner.java
@@ -2,8 +2,12 @@ package com.trubuzz.trubuzz.feature;
 
 import android.os.Bundle;
 import android.support.test.runner.AndroidJUnitRunner;
+import android.util.Log;
+
+import com.trubuzz.trubuzz.utils.God;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by king on 2016/9/26.
@@ -12,25 +16,26 @@ import java.util.ArrayList;
 public class AdRunner extends AndroidJUnitRunner {
     private final String TAG = "jcd_AdRunner";
 
-//    private long startTime;
-//    private long stopTime;
+    private long startTime;
+    private long stopTime;
 
     @Override
     public void onCreate(Bundle arguments) {
-//        this.startTime = new Date().getTime();
+        super.onCreate(arguments);      //这里得先调用, 否则不能初始化startTime , 其他好像不受影响
+
+        this.startTime = new Date().getTime();
         CreateReport.createTestReport();
         CreateReport.getTestReport().setTestClasses(new ArrayList<TestReport.TestClass>());
-//        Log.i(TAG,"onCreate : 创建测试报告对象 at " + God.getDateFormat(startTime));
-        super.onCreate(arguments);
+        Log.i(TAG,"onCreate : 创建测试报告对象 at " + God.getDateFormat(startTime));
     }
 
     @Override
     public void finish(int resultCode, Bundle results) {
-//        this.stopTime = new Date().getTime();
-//        Log.i(TAG, "finish: 测试完成 ,输出测试报告 at "+ God.getDateFormat(stopTime));
+        this.stopTime = new Date().getTime();
+        Log.i(TAG, "finish: 测试完成 ,输出测试报告 at "+ God.getDateFormat(stopTime));
 
-//        CreateReport.getTestReport().setSpendTime(stopTime - startTime);
+        CreateReport.getTestReport().setSpendTime(stopTime - startTime);
         CreateReport.testOutputReport();
-        super.finish(resultCode , results);
+        super.finish(resultCode , results);         //这里得最后调用 , 因为将忽略其后的语句.
     }
 }
