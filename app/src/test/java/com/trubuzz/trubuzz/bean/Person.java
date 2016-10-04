1bfb96ba13ed3e1d793a36d01b8ea271e0a42a63
rawqing <kingstar95d@gmail.com>
2016-10-04 22:31:13 +0800

    决定使用第三方库解决每个方法不同参数问题, 已在test中实现 , 待实际加入使用

---
+++ app/src/test/java/com/trubuzz/trubuzz/bean/Person.java
@@ -0,0 +1,21 @@
+package com.trubuzz.trubuzz.bean;
+
+/**
+ * Created by king on 16/10/4.
+ */
+
+public class Person {
+    private int age;
+    private String name;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public boolean isAdult() {
+        if(this.age >=18){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/test/java/com/trubuzz/trubuzz/test/PT1.java
@@ -0,0 +1,33 @@
+package com.trubuzz.trubuzz.test;
+
+import com.trubuzz.trubuzz.bean.Person;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by king on 16/10/4.
+ */
+@RunWith(JUnitParamsRunner.class)
+public class PT1 {
+
+    @Test
+    @Parameters({"17, false",
+            "22, true" })
+    public void personIsAdult(int age, boolean valid) throws Exception {
+        assertThat(new Person(age).isAdult(), is(valid));
+    }
+
+    @Test
+    @Parameters({"19, false",
+            "2, true" })
+    public void personIsNotAdult(int age, boolean valid) throws Exception {
+        assertThat(new Person(age).isAdult(), is(valid));
+    }
+}
