c6e983f7f59e89cfb366c5c0f1fb3ad5ffb4a73b
rawqing <kingstar95d@gmail.com>
2016-12-08 14:18:33 +0800

    去除多余action

--- app/src/androidTest/java/com/trubuzz/trubuzz/feature/custom/CustomViewAction.java
+++ app/src/androidTest/java/com/trubuzz/trubuzz/feature/custom/CustomViewAction.java
@@ -2,16 +2,12 @@ package com.trubuzz.trubuzz.feature.custom;
 
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
 import org.hamcrest.Matcher;
 
-import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static org.hamcrest.Matchers.allOf;
 
 /**
  * Created by king on 16/10/24.
@@ -41,26 +37,4 @@ public class CustomViewAction  {
        };
    }
 
-    public static ViewAction scrollToRecyclerPosition(int position) {
-        return new ViewAction() {
-            @Override
-            public Matcher<View> getConstraints() {
-                return allOf(isAssignableFrom(RecyclerView.class), isDisplayed());
-            }
-
-            @Override
-            public String getDescription() {
-                return "scroll RecyclerView to position: " + position;
-            }
-
-            @Override
-            public void perform(UiController uiController, View view) {
-                RecyclerView recyclerView = (RecyclerView) view;
-                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
-                layoutManager.scrollToPositionWithOffset(position, 0);
-//                recyclerView.getLayoutManager().smoothScrollToPosition(recyclerView, null, position);
-
-            }
-        };
-    }
 }
